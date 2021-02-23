aa503d35d40bba0831ad7bc5072fa1c2152c24c5
augus99 <augustocastelo1999@gmail.com>
2021-02-22 21:29:08 -0300

    Now the application shows an error if is opened twice

--- Program.cs
+++ Program.cs
@@ -1,13 +1,24 @@
 using System;
+using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Winlogg {
     static class Program {
         [STAThread] static void Main() {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new App());
+            using (Mutex mutex = new Mutex(false, Assembly.GetExecutingAssembly().GetHashCode().ToString())) {
+                if (!mutex.WaitOne(0, false)) {
+                    MessageBox.Show($"The application is already running on another process.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                GC.Collect();
+                
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new App());
+            }
         }
     }
 }
